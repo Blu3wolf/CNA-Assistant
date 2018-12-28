f0fe9a4b8f9cfa04ea443135263a96e836b3ac1c
Blu3wolf <blue@blu3wolf.com>
2018-12-28 08:54:29 +0800

    add unit class, unit types

---
+++ CNA-Assistant/Unit.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNA_Assistant
+{
+    class Unit
+    {
+        public int ID;
+
+        public int CharacteristicsCode;
+
+        public string ShortDesignation;
+
+        public string Designation;
+
+        public int CapabilityPointAllowance;
+
+        public int StackingPoints;
+
+        public UnitType UnitType;
+
+        public IDictionary<TOEStrengthPointType, int> TOEStrengthPoints;
+
+        public int BarrageRating;
+
+        public int VulnerabilityRating;
+
+        public int AntiArmorRating;
+
+        public int ArmorProtectionRating;
+
+        public int OffensiveCloseAssaultRating;
+
+        public int DefensiveCloseAssaultRating;
+
+        public int AntiAircraftRating;
+
+        public int MaxTOEStrengthRating;
+
+        public int BasicMoraleRating;
+
+        public int CurrentMoraleRating;
+
+        public int CapabilityPointsExpended;
+
+        public int CohesionLevel;
+
+        public int ActualBarragePoints;
+
+        public int ActualOffensiveCloseAssaultPoints;
+
+        public int ActualDefensiveCloseAssaultPoints;
+
+        public int FuelReserves;
+
+        public int AmmoReserves;
+
+        public int BreakdownPoints;
+
+        public bool IsAssigned;
+
+        public Unit AssignedToParent;
+
+        public bool IsAttached;
+
+        public Unit AttachedToParent;
+
+        public bool HasAssignedUnits;
+
+        public IList<Unit> AssignedUnits;
+
+        public bool HasAttachedUnits;
+
+        public IList<Unit> AttachedUnits;
+
+        public int 
+
+        public string Notes;
+
+
+    }
+}
---
+++ CNA-Assistant/UnitType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNA_Assistant
+{
+    enum UnitType
+    {
+        InfantryType, TankType, RecceType, ArtilleryType, AntiTankType, AntiAircraftType, Headquarters, Engineers, TankRecoverySquadron, SquadronGroundSupportUnit, DummyTankFormation, Truck
+    }
+}
