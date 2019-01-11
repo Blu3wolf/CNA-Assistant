a357fdbb2154effab09b43aba2fc3937122dbac8
Blu3wolf <blue@blu3wolf.com>
2019-01-11 17:33:15 +0800

    added commands for the axis user naval convoy planning phase

--- CNA-Assistant/IGameCommands.cs
+++ CNA-Assistant/IGameCommands.cs
@@ -24,11 +24,25 @@ namespace CNA_Assistant
 
 		int[] AxisMaltaRaidLevel(int raidLevel, DiceRoll dice1, DiceRoll dice2); // Axis user decides what level of raid on malta they want to do (level I to IV). Throws exception if raidLevel is too high (unavailable). Returns an array of two numbers - percentages of planes available for the raid. Throws exception if user is not axis.
 
-		void AxisAssignMaltaRaid(Squadron squadron, /* Specific Malta Airfield to target */ ); // Axis user assigns squadrons to a specific target for imminent Malta Raid. Requires aircraft from that Squadron to have been assigned to Malta Raids already. 
+		void AxisAssignMaltaRaid(Squadron squadron, /* should this be an enum? */ int maltaTargetID); // Axis user assigns squadrons to a specific target for imminent Malta Raid. Requires aircraft from that Squadron to have been assigned to Malta Raids already. 
 
 		void AssignStrategicMission(Aircraft aircraft, bool isNavalCombat); // user assigns specific aircraft either to Naval recon or Naval bombing (if CW) or Malta Raids or Naval protection (if Axis). True for Bombing Reserve (CW) and Naval CAP (Axis), false for Naval Recon (CW) and Malta Raids (Axis).
 
 		// Probably a bunch of decisions are required by the User to resolve Malta Raids. Air to Air combat, Flak, Bombing and Strafing, at least. Those need to be added at some point.
+
+		/* Naval Convoy Scheduling : CW player plans Replacement Point Production arrivals for next month, in the first phase of THIS month. i.e., Jun I, plan Aug I, II, III and IV. 
+		 * These can be scheduled to arrive during any particular operations stage in that month.
+		 * 
+		 * Axis player plans at least two weeks (full game turns) in advance for Replacement Point arrivals. Replacement Point arrivals happen in a given operations stage, and planned in the ops stage also...
+		 * The planning happens in the first Ops Stage of the turn, not the naval convoy schedule phase. */
+
+		int AxisGetNextShippingLimit(DiceRoll diceRoll); // the diceroll limits the shipping for next turn, thus altering the game state - making it a command. Returns the shipping limit.
+
+		// The axis player assigns shipping within that shipping limit. They divide the shipping limit into convoys as they like, selecting the contents of that convoy from unlimited Stores, Fuel, and Ammunition.
+
+		void AxisDefineConvoy(/* this should definitely be an enum */ int shippingLaneID, int arrivingOpsStage, int ammo, int fuel, int stores, int[] trucks, Unit[] replacements); // trucks[] is an array containing numbers of light, medium and heavy trucks on board. replacements[] is more or less a placeholder for a list of replacement points of many different types.
+
+		// Convoy Resolution Phase decisions/input
 
 
 	}
