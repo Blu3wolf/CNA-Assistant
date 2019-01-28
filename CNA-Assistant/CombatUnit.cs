137215be833257127139e7e8376ff05d8fbd8ed2
Blu3wolf <blue@blu3wolf.com>
2019-01-28 23:00:20 +0800

    push down members of unit to combatunit

---
+++ CNA-Assistant/CombatUnit.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNA_Assistant
+{
+	class CombatUnit : Unit
+	{
+		// properties
+
+		public UnitCharacteristics UnitCharacteristics { get; }
+
+		public int StackingPoints { get; }
+
+		public string ShortDesignation { get; }
+
+		public string Designation { get; }
+
+		public bool IsShell { get; }
+
+		public Unit AttachedTo { get; private set; }
+
+		public List<Unit> AssignedUnits { get; private set; }
+
+		public List<Unit> AttachedUnits { get; private set; }
+
+		public int CohesionLevel { get; private set; }
+
+		public override int CapabilityPointsAllowance
+		{
+			get
+			{
+				// CPA is either: CPA of its UnitCharacteristics; CPA of its attached TOE Gun or Tank points (if lower, and only if UnitCharacteristics does not force its CPA); 
+				// or CPA of an attached unit (if that is lower); or CPA of the Trucks the unit is riding in (if Inf., and Motorised). 
+
+				int CPA = UnitCharacteristics.CapabilityPointAllowance;
+
+				if (true) // all infantry TOE points are motorized by Trucks
+				{
+					// then CPA = attached Truck's lowest CPA (possibly higher than base Unit Characteristic CPA)
+				}
+
+				if (UnitCharacteristics.TOESlowsUnit)
+				{
+					if (true) // if attached TOE Strength Points (Gun or Tank) have lower CPA
+					{
+						// then CPA = attached TOE Strength Point lowest CPA
+					}
+				}
+
+				if (true) // if attached child Unit has lower CPA
+				{
+					// then CPA = that attached units lower CPA
+				}
+
+				if (true) // if attached Trucks have lower CPA
+				{
+					// then CPA = that attached Truck's lower CPA
+				}
+
+				return CPA;
+			}
+		}
+
+		// methods
+
+		override public bool CanMove() // or attack, or defend...
+		{
+			if (CohesionLevel > -26) // check if in valid movement phase? Movement (and Reaction), and Retreat Before Assault. 
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public bool CanAttach(Unit unit) // returns true if able to attach the specified unit to this unit
+		{
+			// This is a complicated chapter. 
+
+			/* So you can attach a unit if there is space in the formation organisation chart, or if there is not space in the org chart, then if there is space in the max attachment chart.
+			 * The problem there is that there are lots and lots of different formation org charts for different types of units. So its not going to be that simple a function to figure out
+			 * whether or not a given unit can be attached. 
+			 * 
+			 * The immediate solution is to only track whether or not a unit can be attached over and above existing assignments - 
+			 * i.e., assume for now that units being attached to have already got a full org chart
+			 * and could not therefore accept any further assignments.
+			 * 
+			 * For now, just return true - implement later.
+			 */
+
+			return true;
+		}
+
+		public bool IsAttached(Unit unit)
+		{
+			if (AttachedUnits.Contains(unit))
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public void Assign() // assigns this unit to the unit it is attached to, if able
+		{
+
+		}
+
+		public void Victory() // called when enemy is pushed out of hex due to combat (not due to reaction, retreat before assault, etc).
+		{
+			CohesionLevel += 3;
+		}
+
+		public void Attach(Unit unit)
+		{
+			if (CanAttach(unit))
+			{
+				AttachedUnits.Add(unit);
+
+				if (AssignedUnits.Contains(unit)) // then both units expend 1 CP
+				{
+
+				}
+				else // both units expend 2 CP, and it must currently be a Reorganisation segment
+				{
+
+				}
+			}
+		}
+
+		public void Detach(Unit unit)
+		{
+			if (IsAttached(unit))
+			{
+				AttachedUnits.Remove(unit);
+
+				if (AssignedUnits.Contains(unit)) // then both units expend 1 CP
+				{
+
+				}
+				else // both units expend 2 CP, and it must currently be a Reorganisation segment or movement segment
+				{
+
+				}
+			}
+		}
+
+		public void Assign(Unit unit)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsAssignable(Unit unit)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Rest() // recover up to 5 cohesion if negative and you didnt work this turn - and not undergoing training!
+		{
+			if (CapabilityPointsExpended == 0)
+			{
+				if (CohesionLevel < 0)
+				{
+					CohesionLevel += 5;
+					if (CohesionLevel > 0)
+					{
+						CohesionLevel = 0;
+					}
+				}
+			}
+		}
+
+		internal void Defeat() // called internally by assault class. If unit takes 30% losses from that assault, or if the unit is forced to retreat due to not assigning any points to defend against an assault.
+		{
+			CohesionLevel -= 3;
+		}
+
+		private void ExpendCapabilityPoints(int points)
+		{
+			CapabilityPointsExpended += points;
+			if (CapabilityPointsExpended > CapabilityPointsAllowance)
+			{
+				int diff = CapabilityPointsExpended - CapabilityPointsAllowance;
+				if (diff >= points)
+				{
+					CohesionLevel -= points;
+				}
+				else
+				{
+					CohesionLevel -= diff;
+				}
+			}
+		}
+	}
+}
--- CNA-Assistant/Unit.cs
+++ CNA-Assistant/Unit.cs
@@ -8,64 +8,13 @@ namespace CNA_Assistant
 {
 	abstract class Unit : ICapabilityPoints
 	{
-		public UnitCharacteristics UnitCharacteristics { get; }
-
 		public int Location { get; private set; }
 
-		public string ShortDesignation { get; }
+		public int CapabilityPointsExpended { get; protected set; }
 
-		public string Designation { get; }
+		abstract public int CapabilityPointsAllowance { get; }
 
-		public bool IsShell { get; }
-
-		public int CapabilityPointsExpended { get; private set; }
-
-		public int CapabilityPointsAllowance
-		{
-			get
-			{
-				// CPA is either: CPA of its UnitCharacteristics; CPA of its attached TOE Gun or Tank points (if lower, and only if UnitCharacteristics does not force its CPA); 
-				// or CPA of an attached unit (if that is lower); or CPA of the Trucks the unit is riding in (if Inf., and Motorised). 
-
-				int CPA = UnitCharacteristics.CapabilityPointAllowance;
-
-				if (true) // all infantry TOE points are motorized by Trucks
-				{
-					// then CPA = attached Truck's lowest CPA (possibly higher than base Unit Characteristic CPA)
-				}
-
-				if (UnitCharacteristics.TOESlowsUnit)
-				{
-					if (true) // if attached TOE Strength Points (Gun or Tank) have lower CPA
-					{
-						// then CPA = attached TOE Strength Point lowest CPA
-					}
-				}
-
-				if (true) // if attached child Unit has lower CPA
-				{
-					// then CPA = that attached units lower CPA
-				}
-
-				if (true) // if attached Trucks have lower CPA
-				{
-					// then CPA = that attached Truck's lower CPA
-				}
-
-				return CPA;
-			}
-		}
-		public int CohesionLevel { get; private set; }
-
-		public int BreakdownPoints { get; private set; }
-
-		public int StackingPoints {	get; }
-
-		public Unit AttachedTo { get; private set; }
-
-		public List<Unit> AssignedUnits { get; private set; }
-
-		public List<Unit> AttachedUnits { get; private set; }
+		public int BreakdownPoints { get; protected set; }
 
 		public int Stores { get; private set; }
 
@@ -75,18 +24,11 @@ namespace CNA_Assistant
 
 		public int Water { get; private set; }
 
-	// methods
+		// methods
 
-	public bool CanMove() // or attack, or defend...
-		{
-			if (CohesionLevel > -26) // check if in valid movement phase? Movement (and Reaction), and Retreat Before Assault. 
-			{
-				return true;
-			}
-			return false;
-		}
+		abstract public bool CanMove(); // depends whether the Unit is a TruckConvoy or a CombatUnit (or a Collection of ReplacementPoints?)
 
-		public void MoveTo(int location, int cpa) // should only happen for Movement (and Reaction), and Retreat Before Assault. 
+		public void MoveTo(int location, int cpa) // would be great if we could pass in a Stack<Hex>, and from that determine CapabilityPoints and BreakdownPoints dynamically.
 		{
 			if (CanMove())
 			{
@@ -102,134 +44,13 @@ namespace CNA_Assistant
 
 		}
 
-		public void Assign() // assigns this unit to the unit it is attached to, if able
+		internal void EndOpsStage() // Should happen in response to an event at end of operations stage.
 		{
-
-		}
-
-		private void ExpendCapabilityPoints(int points)
-		{
-			CapabilityPointsExpended += points;
-			if (CapabilityPointsExpended > CapabilityPointsAllowance)
-			{
-				int diff = CapabilityPointsExpended - CapabilityPointsAllowance;
-				if (diff >= points)
-				{
-					CohesionLevel -= points;
-				}
-				else
-				{
-					CohesionLevel -= diff;
-				}
-			}
-		}
-
-		public void Victory() // called when enemy is pushed out of hex due to combat (not due to reaction, retreat before assault, etc).
-		{
-			CohesionLevel += 3;
-		}
-
-		internal void Defeat() // called internally by assault class. If unit takes 30% losses from that assault, or if the unit is forced to retreat due to not assigning any points to defend against an assault.
-		{
-			CohesionLevel -= 3;
-		}
-
-		private void Rest() // recover up to 5 cohesion if negative and you didnt work this turn - and not undergoing training!
-		{
-			if (CapabilityPointsExpended == 0)
-			{
-				if (CohesionLevel < 0)
-				{
-					CohesionLevel += 5;
-					if (CohesionLevel > 0)
-					{
-						CohesionLevel = 0;
-					}
-				}
-			}
-		}
-
-		internal void EndOpsStage() // Should happen in response to an event at end of operations stage (both sides).
-		{
-			Rest();
-
 			CapabilityPointsExpended = 0;
-
 			BreakdownPoints = 0;
 		}
 
-		public bool CanAttach(Unit unit) // returns true if able to attach the specified unit to this unit
-		{
-			// This is a complicated chapter. 
-
-			/* So you can attach a unit if there is space in the formation organisation chart, or if there is not space in the org chart, then if there is space in the max attachment chart.
-			 * The problem there is that there are lots and lots of different formation org charts for different types of units. So its not going to be that simple a function to figure out
-			 * whether or not a given unit can be attached. 
-			 * 
-			 * The immediate solution is to only track whether or not a unit can be attached over and above existing assignments - 
-			 * i.e., assume for now that units being attached to have already got a full org chart
-			 * and could not therefore accept any further assignments.
-			 * 
-			 * For now, just return true - implement later.
-			 */
-
-			return true;
-		}
-
-		public void Attach(Unit unit)
-		{
-			if (CanAttach(unit))
-			{
-				AttachedUnits.Add(unit);
-
-				if (AssignedUnits.Contains(unit)) // then both units expend 1 CP
-				{
-
-				}
-				else // both units expend 2 CP, and it must currently be a Reorganisation segment
-				{
-
-				}
-			}
-		}
-
-		public void Detach(Unit unit)
-		{
-			if (IsAttached(unit))
-			{
-				AttachedUnits.Remove(unit);
-
-				if (AssignedUnits.Contains(unit)) // then both units expend 1 CP
-				{
-
-				}
-				else // both units expend 2 CP, and it must currently be a Reorganisation segment or movement segment
-				{
-
-				}
-			}
-		}
-
-		public bool IsAttached(Unit unit)
-		{
-			if (AttachedUnits.Contains(unit))
-			{
-				return true;
-			}
-			return false;
-		}
-
-		public void Assign(Unit unit)
-		{
-			throw new NotImplementedException();
-		}
-
-		public bool IsAssignable(Unit unit)
-		{
-			throw new NotImplementedException();
-		}
-
-		internal void Evaporate(Game.Evaporation evaporation)
+		internal void Evaporate(Game.Evaporation evaporation) // override in CombatUnit?
 		{
 			int fuelEvaporate = Fuel;
 			int waterEvaporate = Water;
@@ -248,8 +69,17 @@ namespace CNA_Assistant
 						waterEvaporate *= 6;
 					}
 					break;
-				case Game.Evaporation.HotWeather:
+				case Game.Evaporation.HotWeather: // do not count fuel and water in the tanks and radiators
 					{
+						// not all units have TOEStrengthPoints to get a Fuel Capacity from. 
+						// all units may have attached Trucks (CombatUnits have 1st line trucks, TruckConvoys have 2nd/3rd line trucks)
+
+						// each vehicle TOE point has 1 Water in the radiator - but not all units have TOE points. 
+						// each Truck point has 1 water in the radiator
+
+						fuelEvaporate -= fuelintanks;
+						waterEvaporate -= waterinradiators;
+
 						fuelEvaporate *= 5;
 						waterEvaporate *= 5;
 					}
