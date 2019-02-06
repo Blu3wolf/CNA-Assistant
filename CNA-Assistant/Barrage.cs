7df9b09546a869471f2ef04a7c49ca73f224e3b2
Blu3wolf <blue@blu3wolf.com>
2019-02-06 21:54:31 +0800

    Add classes for barrage, barrage step and barrage target

---
+++ CNA-Assistant/Barrage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNA_Assistant
+{
+	class Barrage
+	{
+		public ReadOnlyCollection<CombatUnit> FiringUnits { get; }
+
+		public int ActualBarragePoints { get; }
+	}
+}
---
+++ CNA-Assistant/BarrageStep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNA_Assistant
+{
+	class BarrageStep
+	{
+		public List<CombatUnit> FiringUnits;
+
+		public List<CombatUnit> UnitsInContact;
+
+		public List<BarrageEffects> 
+	}
+}
