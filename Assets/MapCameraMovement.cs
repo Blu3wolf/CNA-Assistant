e5dd04139c0727ec89248e6f2a293317f3c61196
Blu3wolf <blue@blu3wolf.com>
2020-04-08 17:44:40 +0800

    initial unity commit

---
+++ Assets/MapCameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class MapCameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float panspeed = 10.0f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            panspeed = 25.0f;
+        }
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector2 position = transform.position;
+        position.x += panspeed * horizontal * Time.deltaTime;
+        position.y += panspeed * vertical * Time.deltaTime;
+
+        transform.position = position;
+    }
+}
